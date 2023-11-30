e836c0e0ef6136076ee333fd432e9b2a1314a28c
Bhopal Singh <bhopalsinghsis@gmail.com>
2023-11-30 11:49:17 -0500

    Add Query and Mutation - AddDay, RemoveDay

--- graphql-with-apollo-server/index.js
+++ graphql-with-apollo-server/index.js
@@ -1,8 +1,41 @@
 const { ApolloServer, gql } = require("apollo-server");
 
 const typeDefs = gql`
+  type SkiDay {
+    id: ID!
+    date: String!
+    mountain: String!
+    conditions: Conditions!
+  }
+
+  enum Conditions {
+    POWDER
+    HEAVY
+    ICE
+    THIN
+  }
+
+  input AddDayInput {
+    date: String!
+    mountain: String!
+    conditions: Conditions
+  }
+
+  type RemoveDayPayload {
+    day: SkiDay!
+    removed: Boolean
+    totalBefore: Int
+    totalAfter: Int
+  }
+
   type Query {
     totalDays: Int!
+    allDays: [SkiDay!]!
+  }
+
+  type Mutation {
+    removeDay(id: ID): RemoveDayPayload!
+    addDay(input: AddDayInput): SkiDay
   }
 `;
 
